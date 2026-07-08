ea0404cf8244097b1f9316077cf45600fc1790f8
EdgarCastro <cafucastro12@gmail.com>
2026-07-08 13:12:54 +0100

    Modify products.

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace SuperShop.Data.Entities
 {
@@ -7,19 +9,27 @@ namespace SuperShop.Data.Entities
     {
         public int Id { get; set; }
 
+
+        [Required]
+        [MaxLength(50, ErrorMessage="The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
+
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)] //formata em modo moeda mas em modo de ecição no faz nenhuma formatação
         public decimal Price { get; set; }
 
+
         [Display(Name = "Image")] //Como aparece na web (nome do campo)
         public string ImageUrl { get; set; }
+       
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
+
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
+
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
