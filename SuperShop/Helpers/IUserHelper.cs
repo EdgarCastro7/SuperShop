34b61877d154aca308d523a100e5359b12147c1b
EdgarCastro <cafucastro12@gmail.com>
2026-08-19 10:40:21 +0100

    Change user profile and password

--- SuperShop/Helpers/IUserHelper.cs
+++ SuperShop/Helpers/IUserHelper.cs
@@ -14,5 +14,9 @@ namespace SuperShop.Helpers
         Task<SignInResult> LogInAsync(LoginViewModel model);
 
         Task LogoutAsync();
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
     }
 }
