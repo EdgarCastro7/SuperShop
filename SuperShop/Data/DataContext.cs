93f03d66bbda22734bd2494c73852c9909ee7722
EdgarCastro <cafucastro12@gmail.com>
2026-09-07 09:52:49 +0100

    Cascade Delete Rule

--- SuperShop/Data/DataContext.cs
+++ SuperShop/Data/DataContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
+using System.Linq;
 
 namespace SuperShop.Data
 {
@@ -19,5 +21,21 @@ namespace SuperShop.Data
         {
 
         }
+
+        //Habilitar a regra de apagar em cascata (Cascade Delete Rule)
+        //protected override void OnModelCreating(ModelBuilder modelBuilder) //apagar em cascata
+        //{
+        //    var cascadeFKs = modelBuilder.Model
+        //        .GetEntityTypes() //Vai buscar todas as tabelas
+        //        .SelectMany(t => t.GetForeignKeys()) //Seleciona todas as FKs
+        //        .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade); //Aqui procuramos todas as FKs que tenham o comportamento cascata
+            
+        //    foreach (var fk in cascadeFKs)
+        //    {
+        //        fk.DeleteBehavior = DeleteBehavior.Cascade;
+        //    }
+
+        //    base.OnModelCreating(modelBuilder);
+        //}
     }
 }
